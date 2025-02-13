b8addf57b4e468474dcec8c4f3caa84c0ceae772
michaeljymsgutierrez <leeez711@gmail.com>
2025-02-13 13:35:37 +0800

    Removed api directory

--- src/app/api/v1/shops/route.ts
+++
@@ -1,7 +0,0 @@
-import generateShopsData from '@utils/generate-shops-data'
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(request: Request, response: Response) {
-  const data = generateShopsData()
-  return Response.json(data)
-}
